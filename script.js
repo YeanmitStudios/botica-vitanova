8d4ecb94a58d2eb79a781ecea3970e7d1818a547
YeanmitStudios <anthonymurilloy.0104@gmail.com>
2026-06-27 16:07:53 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     
-    navLinks.classList.toggle('active');
+    // Si ya está activo, lo quitamos, si no, lo ponemos
+    if (navLinks.style.display === 'flex') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'flex';
+        navLinks.style.animation = 'fadeIn 0.3s ease';
+    }
 }
